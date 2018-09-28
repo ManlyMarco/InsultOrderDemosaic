1e322c3efc2846d6a12b0e0d79ae411570518235
MarC0 <dummyaccx@gmail.com>
2018-09-28 22:22:46 +0200

    First release

---
+++ InsultOrderDemosaic/Demosaic.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using Harmony;
+
+namespace InsultOrderDemosaic
+{
+    [BepInPlugin(Guid, "Demosaic", "1.0")]
+    public class Demosaic : BaseUnityPlugin
+    {
+        private const string Guid = "io.marc0.demosaic";
+
+        public Demosaic()
+        {
+            Hooks.InstallHooks(Guid);
+        }
+    }
+
+    internal static class Hooks
+    {
+        public static void InstallHooks(string guid)
+        {
+            var harmony = HarmonyInstance.Create(guid);
+            harmony.PatchAll(typeof(Hooks));
+        }
+        
+        [HarmonyPrefix, HarmonyPatch(typeof(MozaicSetUp), "Update")]
+        public static bool UpdateHook(MozaicSetUp __instance)
+        {
+            if (__instance.MozaObj != null) __instance.MozaObj.enabled = false;
+            return false;
+        }
+    }
+}
